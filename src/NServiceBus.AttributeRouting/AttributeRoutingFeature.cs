4757e460ffc3671e3d0acdb92648cd3eb26485f4
mauroservienti <mauro.servienti@gmail.com>
2019-06-11 16:39:55 +0200

    stop using a FeatureStartupTask

--- src/NServiceBus.AttributeRouting/AttributeRoutingFeature.cs
+++ src/NServiceBus.AttributeRouting/AttributeRoutingFeature.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace NServiceBus.AttributeRouting
 {
@@ -24,60 +23,35 @@ namespace NServiceBus.AttributeRouting
                 .GetMethod("GetAllMessages", BindingFlags.Instance | BindingFlags.NonPublic)
                 .Invoke(messageMetadataRegistry, null);
 
-            context.RegisterStartupTask(new AttributeRoutingSetup(unicastRoutingTable, allMessageMetadata));
+            var routes = new List<RouteTableEntry>();
+            foreach (var messageMetadata in allMessageMetadata)
+            {
+                if (IsRouteDefinedFor(messageMetadata.MessageType, unicastRoutingTable))
+                {
+                    continue;
+                }
+
+                var routeTo = messageMetadata.MessageType.GetCustomAttribute<RouteToAttribute>();
+                if (routeTo != null)
+                {
+                    routes.Add(new RouteTableEntry(messageMetadata.MessageType, UnicastRoute.CreateFromEndpointName(routeTo.Destination)));
+                }
+            }
+
+            if (routes.Any())
+            {
+                unicastRoutingTable.AddOrReplaceRoutes("AttributeRoutingSource", routes);
+            }
         }
 
-        class AttributeRoutingSetup : FeatureStartupTask
+        bool IsRouteDefinedFor(Type messageType, UnicastRoutingTable unicastRoutingTable)
         {
-            private UnicastRoutingTable unicastRoutingTable;
-            private IEnumerable<MessageMetadata> allMessageMetadata;
+            //this could be a FastDelegate invocation
+            var unicastRoute = (UnicastRoute)typeof(UnicastRoutingTable)
+                .GetMethod("GetRouteFor", BindingFlags.Instance | BindingFlags.NonPublic)
+                .Invoke(unicastRoutingTable, new[] { messageType });
 
-            public AttributeRoutingSetup(UnicastRoutingTable unicastRoutingTable, IEnumerable<MessageMetadata> allMessageMetadata)
-            {
-                this.unicastRoutingTable = unicastRoutingTable;
-                this.allMessageMetadata = allMessageMetadata;
-            }
-
-            protected override Task OnStart(IMessageSession session)
-            {
-                var routes = new List<RouteTableEntry>();
-
-                foreach (var messageMetadata in allMessageMetadata)
-                {
-                    if (IsRouteDefinedFor(messageMetadata.MessageType, unicastRoutingTable))
-                    {
-                        continue;
-                    }
-
-                    var routeTo = messageMetadata.MessageType.GetCustomAttribute<RouteToAttribute>();
-                    if (routeTo != null)
-                    {
-                        routes.Add(new RouteTableEntry(messageMetadata.MessageType, UnicastRoute.CreateFromEndpointName(routeTo.Destination)));
-                    }
-                }
-
-                if (routes.Any())
-                {
-                    unicastRoutingTable.AddOrReplaceRoutes("AttributeRoutingSource", routes);
-                }
-
-                return Task.CompletedTask;
-            }
-
-            protected override Task OnStop(IMessageSession session)
-            {
-                return Task.CompletedTask;
-            }
-
-            bool IsRouteDefinedFor(Type messageType, UnicastRoutingTable unicastRoutingTable)
-            {
-                //this could be a FastDelegate invocation
-                var unicastRoute = (UnicastRoute)typeof(UnicastRoutingTable)
-                    .GetMethod("GetRouteFor", BindingFlags.Instance | BindingFlags.NonPublic)
-                    .Invoke(unicastRoutingTable, new[] { messageType });
-
-                return unicastRoute != null;
-            }
+            return unicastRoute != null;
         }
     }
 }
