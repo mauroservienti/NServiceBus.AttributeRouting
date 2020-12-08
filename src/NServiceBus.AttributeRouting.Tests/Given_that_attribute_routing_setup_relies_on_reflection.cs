44c0fec81c275e92137e6bf95386a47c4f7eac01
Mauro Servienti <mauro.servienti@gmail.com>
2020-12-08 19:02:32 +0100

    MessageMetadataRegistry is not needed

--- src/NServiceBus.AttributeRouting.Tests/Given_that_attribute_routing_setup_relies_on_reflection.cs
+++ src/NServiceBus.AttributeRouting.Tests/Given_that_attribute_routing_setup_relies_on_reflection.cs
@@ -9,17 +9,6 @@ namespace NServiceBus.AttributeRouting.Tests
     public class Given_that_attribute_routing_setup_relies_on_reflection
     {
         [Test]
-        public void Make_sure_message_metadata_registry_doesnt_change()
-        {
-            var method = typeof(MessageMetadataRegistry)
-                .GetMethod("GetAllMessages", BindingFlags.Instance | BindingFlags.NonPublic);
-
-
-            Assert.False(method == null, "Cannot find GetAllMessages method on MessageMetadataRegistry type");
-            Assert.True(method.ReturnType == typeof(IEnumerable<MessageMetadata>), "The return type is not the expected IEnumerable<MessageMetadata>.");
-        }
-
-        [Test]
         public void Make_sure_unicast_routing_table_GetRouteFor_doesnt_change()
         {
             var method = typeof(UnicastRoutingTable)
