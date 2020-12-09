63c4225302a0abb94d12803016d529aa487d77c1
mauroservienti <mauro.servienti@gmail.com>
2020-12-09 11:31:13 +0100

    types scanner tests

---
+++ src/NServiceBus.AttributeRouting.Tests/ConventionsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace NServiceBus.AttributeRouting.Tests
+{
+    static class ConventionsExtensions
+    {
+        public static void Add(this Conventions conventions, IMessageConvention convention)
+        {
+            var addMethod = typeof(Conventions).GetMethod("Add", BindingFlags.Instance | BindingFlags.NonPublic);
+            addMethod.Invoke(conventions, new object[] {convention});
+        }
+    }
+}
