4a5907c5002e52368ab95a0d5e511d5eef7771b7
mauroservienti <mauro.servienti@gmail.com>
2020-12-09 11:25:12 +0100

    Make feature internal and move scanning to a dedicated class

--- src/NServiceBus.AttributeRouting/AttributeRoutingFeature.cs
+++ src/NServiceBus.AttributeRouting/AttributeRoutingFeature.cs
@@ -1,7 +1,6 @@
 ﻿using NServiceBus.AttributeRouting.Contracts;
 using NServiceBus.Features;
 using NServiceBus.Routing;
-using NServiceBus.Unicast.Messages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +9,7 @@ using NServiceBus.AttributeRouting.AssemblyScanning;
 
 namespace NServiceBus.AttributeRouting
 {
-    public class AttributeRoutingFeature : Feature
+    class AttributeRoutingFeature : Feature
     {
         public AttributeRoutingFeature()
         {
@@ -19,19 +18,11 @@ namespace NServiceBus.AttributeRouting
         protected override void Setup(FeatureConfigurationContext context)
         {
             var coreScannerConfig = context.Settings.Get<AssemblyScannerConfiguration>();
-            var excludedTypes = GetExcludedTypes(coreScannerConfig);
-            var excludedAssemblies = GetExcludedAssemblies(coreScannerConfig);
-            
-            var scanner = new InternalAssemblyScanner();
-            var assemblies = scanner.Scan().ToList();
-            
             var unicastRoutingTable = context.Settings.Get<UnicastRoutingTable>();
             var conventions = context.Settings.Get<Conventions>();
-            
-            var messageTypes = assemblies.SelectMany(a=>a.GetTypes())
-                .Where(t => conventions.IsCommandType(t) || conventions.IsMessageType(t))
-                .ToList();
-            
+
+            var messageTypes = TypesScanner.ScanMessageTypes(coreScannerConfig, conventions);
+
             var routes = new List<RouteTableEntry>();
             foreach (var messageType in messageTypes)
             {
@@ -49,13 +40,13 @@ namespace NServiceBus.AttributeRouting
                 else
                 {
                     var routeAttribute = messageType.Assembly.GetCustomAttribute<RouteAttribute>();
-                    if (conventions.IsCommandType(messageType) 
+                    if (conventions.IsCommandType(messageType)
                         && routeAttribute?.CommandsDestination != null)
                     {
                         routes.Add(new RouteTableEntry(messageType,
                             UnicastRoute.CreateFromEndpointName(routeAttribute.CommandsDestination)));
                     }
-                    else if (conventions.IsMessageType(messageType) 
+                    else if (conventions.IsMessageType(messageType)
                              && routeAttribute?.MessagesDestination != null)
                     {
                         routes.Add(new RouteTableEntry(messageType,
@@ -68,22 +59,6 @@ namespace NServiceBus.AttributeRouting
             {
                 unicastRoutingTable.AddOrReplaceRoutes("AttributeRoutingSource", routes);
             }
-        }
-
-        List<Type> GetExcludedTypes(AssemblyScannerConfiguration configuration)
-        {
-            var property = typeof(AssemblyScannerConfiguration).GetProperty("ExcludedTypes",
-                BindingFlags.Instance | BindingFlags.NonPublic);
-
-            return (List<Type>)property?.GetValue(configuration, null);
-        }
-
-        List<string> GetExcludedAssemblies(AssemblyScannerConfiguration configuration)
-        {
-            var property = typeof(AssemblyScannerConfiguration).GetProperty("ExcludedAssemblies",
-                BindingFlags.Instance | BindingFlags.NonPublic);
-
-            return (List<string>)property?.GetValue(configuration, null);
         }
 
         bool IsRouteDefinedFor(Type messageType, UnicastRoutingTable unicastRoutingTable)
