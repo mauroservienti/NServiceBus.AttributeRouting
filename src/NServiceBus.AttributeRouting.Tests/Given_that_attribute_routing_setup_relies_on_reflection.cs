9b1eba40059661a1da07122b3ae7bd5e4bf9f950
Mauro Servienti <mauro.servienti@gmail.com>
2020-12-09 06:19:40 +0100

    more reflection tests

--- src/NServiceBus.AttributeRouting.Tests/Given_that_attribute_routing_setup_relies_on_reflection.cs
+++ src/NServiceBus.AttributeRouting.Tests/Given_that_attribute_routing_setup_relies_on_reflection.cs
@@ -1,5 +1,5 @@
+using System;
 using NServiceBus.Routing;
-using NServiceBus.Unicast.Messages;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Reflection;
@@ -9,6 +9,26 @@ namespace NServiceBus.AttributeRouting.Tests
     public class Given_that_attribute_routing_setup_relies_on_reflection
     {
         [Test]
+        public void Make_sure_assembly_scanner_configuration_has_ExcludedAssemblies_property()
+        {
+            var property = typeof(AssemblyScannerConfiguration).GetProperty("ExcludedAssemblies",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            
+            Assert.NotNull(property, "Cannot find ExcludedAssemblies property on AssemblyScannerConfiguration type");
+            Assert.True(property.PropertyType==typeof(List<string>),"The return type of ExcludedAssemblies is not the expected List<string>.");
+        }
+        
+        [Test]
+        public void Make_sure_assembly_scanner_configuration_has_ExcludedTypes_property()
+        {
+            var property = typeof(AssemblyScannerConfiguration).GetProperty("ExcludedTypes",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            
+            Assert.NotNull(property, "Cannot find ExcludedTypes property on AssemblyScannerConfiguration type");
+            Assert.True(property.PropertyType==typeof(List<Type>),"The return type of ExcludedTypes is not the expected List<Type>.");
+        }
+
+        [Test]
         public void Make_sure_unicast_routing_table_GetRouteFor_doesnt_change()
         {
             var method = typeof(UnicastRoutingTable)
@@ -16,7 +36,7 @@ namespace NServiceBus.AttributeRouting.Tests
 
 
             Assert.False(method == null, "Cannot find GetRouteFor method on UnicastRoutingTable type");
-            Assert.True(method.ReturnType == typeof(UnicastRoute), "The return type is not the expected UnicastRoute.");
+            Assert.True(method.ReturnType == typeof(UnicastRoute), "The return type of GetRouteFor is not the expected UnicastRoute.");
         }
     }
 }
